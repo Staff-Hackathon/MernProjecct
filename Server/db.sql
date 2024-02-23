e85a8a8d6799f2c54551a271baf45d242dc77cf3
Shubham Borle <shubham.borle@sunbeaminfo.com>
2024-02-23 15:26:20 +0530

    sever submit feedback done

--- Server/db.sql
+++ Server/db.sql
@@ -1,3 +1,14 @@
+npm -v   
+10.2.3
+node -v
+v20.10.0
+
+mysql -u root -pmanager
+
+use staffhackathon;
+show tables;
+
+
 create table user (
     id integer primary key auto_increment,
     firstName varchar(50),
@@ -6,7 +17,8 @@ create table user (
     password varchar(100),
     profileImage varchar(100),
     createdTime timestamp default CURRENT_TIMESTAMP, 
-    role varchar(10)
+    role varchar(10),
+    course varchar(20)
 );
 
 create table feedback (
@@ -15,5 +27,25 @@ create table feedback (
     type varchar(50),
     course varchar(50),
     sdate date,
-    edate date
+    edate date,
+    rating float(3, 2)
 );
+
+create table Tfeedback (
+    tid integer primary key auto_increment,
+    fid integer,
+    sid integer,
+    Punctuality integer,
+    queries_solved integer,
+    Initiative integer,
+    responsiveness integer,
+    FOREIGN KEY (fid) REFERENCES feedback(fid),
+    FOREIGN KEY (sid) REFERENCES user(id)
+);
+
+
+-- To clear mysql terminal screen \! cls
+
+-- npm install bootstrap@5.3.3
+
+
